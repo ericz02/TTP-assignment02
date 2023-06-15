f4305c31a2ad6e6ede057dfef348d5dbdfcb4bf1
ericz02 <ezheng0720@gmail.com>
2023-06-15 14:17:20 -0400

    init

---
+++ myEach.js
@@ -0,0 +1,18 @@
+const pets = ["Cat", "Dog", "Rabbit"];
+
+function myEach(arr, CB) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i] + CB());
+  }
+}
+
+function callbackFunc(){
+  return "s";
+}
+
+// myEach(pets, callbackFunc)
+
+myEach(pets, (s) => "s");
+
+
+
